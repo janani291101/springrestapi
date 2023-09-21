d10e7ed089d984edd1dcd75a342071eed4505d74
pj <pj@PJ01.corp.amdocs.com>
2023-09-21 10:57:15 +0530

    gnk

---
+++ SpringbootRouteAPI/src/test/java/com/amdocs/SpringbootRouteCase/SpringbootRouteCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amdocs.SpringbootRouteCase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRouteCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
